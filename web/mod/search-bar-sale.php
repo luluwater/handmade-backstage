4e99b4e6dc52f886d8da2c8d9c4a8bacb8cdbfd7
zed <z072614@gmail.com>
2022-07-06 14:46:30 +0800

    h

---
+++ web/mod/search-bar-sale.php
@@ -0,0 +1,16 @@
+<form action="">
+    <div class="row  my-4">
+        <div class="col-2">
+            <select class="form-select" aria-label="Default select example">
+                <option value="1" selected>關鍵字</option>
+                <option value="2">活動時間</option>
+            </select>
+        </div>
+        <div class="col-6">
+            <input class="form-control" type="search" name="search" id="">
+        </div>
+        <div class="col-1">
+            <a href="" class="btn btn-bg-color">搜尋</a>
+        </div>
+    </div>
+</form>
