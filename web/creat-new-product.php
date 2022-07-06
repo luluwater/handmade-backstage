e1a4f54814242bdcfd99e3c3d87d0b2aed53d24a
admin <make75682@gmail.com>
2022-07-06 18:42:02 +0800

    connect db and update creat-product.php

--- web/creat-new-product.php
+++ web/creat-new-product.php
@@ -1,3 +1,21 @@
+<?php
+require_once("../db-connect.php");
+$stmt=$db_host->prepare("SELECT * FROM category");
+$stmt_store=$db_host->prepare("SELECT store.id,store.name FROM store WHERE store.category_id = 1");
+
+try{    
+    $stmt->execute();
+    $stmt_store->execute();
+    $rows=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    $rows_store=$stmt_store->fetchAll(PDO::FETCH_ASSOC);
+}catch (PDOException $e){
+    echo "預處理陳述式執行失敗！ <br/>";
+    echo "Error: " . $e->getMessage() . "<br/>";
+    $db_host = NULL;
+    exit;
+}
+$db_host = NULL;
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -37,52 +55,56 @@
                     </div>
                 </div>
                 <div class="my-3 row align-items-center">
-                    <label class="col-1" for="">名稱</label>
-                    <input class="col form-control" type="text">
+                    <label class="col-1" for="product_name">名稱</label>
+                    <input class="col form-control" type="text" name="product_name" placeholder="請輸入商品名稱">
                 </div>
                 <div class="my-3 row align-items-center">
-                    <label class="col-1" for="">簡介</label>
-                    <textarea class="form-control col" name="" id="" cols="30" rows="10" style="resize:none"></textarea>                   
+                    <label class="col-1" for="intro">簡介</label>
+                    <textarea class="form-control col" name="intro" cols="30" rows="10" style="resize:none" placeholder="請輸入商品介紹"></textarea>                   
                 </div>
                 <div class="my-3 row align-items-center">
-                    <label class="col-1" for="">價格</label>
-                    <input class="col form-control" type="number">
+                    <label class="col-1" for="price">價格</label>
+                    <input class="col form-control" type="number" name="price" placeholder="請輸入商品價格">
                 </div>                
-                <div class="my-3 row align-items-center justify-content-between">
-                    <div class="col-5 d-flex">
-                        <label class="col-2 me-2" for="">類別</label>
-                        <select class="form-select col" aria-label="Default select example">
-                        <option value="1">花藝</option>
-                        <option value="2">金工</option>                    
+                <div class="my-3 row justify-content-between">
+                    <div class="col-5 d-flex align-items-center">
+                        <label class="col-2 me-2" for="category">類別</label>
+                        <select id="category" class="form-select col" aria-label="Default select example" name="category">
+                        <?php foreach($rows as $row): ?>
+                        <option value="<?=$row["id"] ?>"><?=$row["category_name"] ?></option>
+                        <?php endforeach; ?>         
                         </select>
                     </div>
-                    <div class="col-5 d-flex pe-0">
-                        <label class="col-2 me-2" for="">品牌</label>
-                        <select class="form-select col" aria-label="Default select example">
-                        <option value="1">store</option>
-                        <option value="2">store</option>                    
+                    <div class="col-5 d-flex pe-0  align-items-center">
+                        <label class="col-2 me-2" for="store">品牌</label>
+                        <select  id="store" class="form-select col" aria-label="Default select example" name="store">
+                        <?php foreach($rows_store as $row): ?>
+                        <option value="<?=$row["id"]?>"><?=$row["name"]?></option>
+                        <?php endforeach; ?>
                         </select>
                     </div>
-                    <div class="col-5 d-flex gy-3">
-                        <label class="col-2 me-2" for="">商品類型</label>
-                        <select class="form-select col" aria-label="Default select example">
-                        <option value="1">體驗課程</option>
-                        <option value="2">實體商品</option>                    
+                    <div class="col-5 d-flex gy-3  align-items-center">
+                        <label class="col-2 me-2" for="type">商品類型</label>
+                        <select id="type" class="form-select col" aria-label="Default select example" name="type">
+                        <option value="體驗課程">體驗課程</option>
+                        <option value="實體商品">實體商品</option>                    
                         </select>
                     </div>
                 </div>                
                 <!--  -->
-                <div class="my-3 row align-items-center">
-                    <label class="col-1" for="">課程日程</label>
-                    <input class="col form-control" type="date">
+                <div id="course">
+                    <div class="my-3 row align-items-center">
+                        <label class="col-1" for="datetime">課程日程</label>
+                        <input class="col form-control" type="datetime-local" name="datetime">
+                    </div>
+                    <div class="my-3 row align-items-center">
+                        <label class="col-1" for="hour">課程時常</label>
+                        <input class="col form-control" type="number" name="hour" placeholder="請輸入課程時常">
+                    </div>
                 </div>
                 <div class="my-3 row align-items-center">
-                    <label class="col-1" for="">課程時常</label>
-                    <input class="col form-control" type="text">
-                </div>
-                <div class="my-3 row align-items-center">
-                    <label class="col-1" for="">注意事項</label>
-                    <textarea class="form-control col" name="" id="" cols="30" rows="10" style="resize:none"></textarea>
+                    <label class="col-1" for="notice">注意事項</label>
+                    <textarea class="form-control col" name="notice" cols="30" rows="10" style="resize:none" placeholder="請輸入商品注意事項"></textarea>
                 </div>
                 <div class="my-5 d-flex justify-content-end">
                     <a href="" class="ms-3 btn btn-bg-color">取消</a>
@@ -92,6 +114,8 @@
         </div>
     </main>
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>    
     <script>
         const previewImages=document.querySelectorAll(".previewImage");
         const upload_images=document.querySelectorAll(".upload_image");
@@ -106,7 +130,47 @@
                 img.src=objUrl;
                 img.onload=()=>window.URL.revokeObjectURL(objUrl);
             })
-        }        
+        }   
+        
+        const type=document.querySelector("#type");
+        const course=document.querySelector("#course");
+        type.addEventListener("change",function(){
+            // console.log("type change");
+            if(this.value=="體驗課程"){
+                course.classList.remove("d-none");
+            }else{
+                course.classList.add("d-none");
+            }
+        })
+
+        const category=document.querySelector("#category");
+        const store=document.querySelector("#store");
+        category.addEventListener("change",function(){
+            // console.log("category change");
+            categoryValue=this.value;
+            // console.log(store.children);
+            for(let i=store.children.length-1;i>=0;i-- ){
+                store.removeChild(store[i]);
+            }
+            $.ajax({
+            	method: "GET",  //or GET
+            	url:  "../api/filte-store.php",
+            	dataType: "json",
+            	data: { category_id: categoryValue } //如果需要
+            	})
+            	.done(function( response ) {
+                	console.log(response);
+                    for(let store of response.stores){
+                        html=document.createElement("option");
+                        html.textContent=store.name;
+                        html.setAttribute("value",store.id);
+                    }                    
+                    store.prepend(html);
+            	}).fail(function( jqXHR, textStatus ) {
+                	console.log( "Request failed: " + textStatus );
+            	});
+        })
+        
     </script>
   </body>
 </html>
--- web/do-create-product.php
+++ web/do-create-product.php
@@ -1,8 +1,55 @@
-<?php
-    $product_img1=$_FILES["product_img1"]["name"];
-    // $product_img2=$_FILES["product_img2"];
-    // $product_img3=$_FILES["product_img3"];
-    // $product_img4=$_FILES["product_img4"];
+<?php  
+    function upFileToDir($dir){
+        for($i=1;$i<=4;$i++){
+            if($_FILES["product_img".$i]["error"]==0){
+                if(move_uploaded_file($_FILES["product_img".$i]["tmp_name"],"./$dir/".$_FILES["product_img".$i]["name"])){
+                    $fileName=$_FILES["product_img".$i]["name"];
+                    $now=date('y-m-d H:i:s');
+                    echo $fileName;                    
+                    echo "success <br>";
+                }else{
+                    echo "fail";
+                }
+            }
+        }
+    }
+    // function upFileTo_db(){
+    //     $sql="INSERT INTO product_img (img_name,product_id) VALUES ('$fileName','product_id')";
+    //                 if(global $conn->query($sql)===true){
+    //                     echo "新資料輸入成功";
+    //                 }else{
+    //                     echo "Error: ".$sql;
+    //                 }                    
+    //                 // header("location: file-upload.php");
+    // }
+    upFileToDir("./test-img");
 
-    echo $product_img1;
+    $productName=$_POST["product_name"];
+    $intro=$_POST["intro"];
+    $price=$_POST["price"];
+    $category=$_POST["category"];
+    $store=$_POST["store"];
+    $type=$_POST["type"];  
+    $notice=$_POST["notice"];
+
+   
+
+    echo "productName: ".$productName."<br>";
+    echo "intro: ".$intro."<br>";
+    echo "price: ".$price."<br>";
+    echo "category: ".$category."<br>";
+    echo "store: ".$store."<br>";
+    echo "type: ".$type."<br>";
+    echo "notice: ".$notice."<br>";
+    if($type === "體驗課程"){
+        $datetime=$_POST["datetime"];
+        $hour=$_POST["hour"];
+        echo "datetime: ".$datetime."<br>";
+        echo "hour:".$hour."<br>";
+    }
+
+    
+    
+   
+
 ?>
