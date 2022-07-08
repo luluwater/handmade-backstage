0c1b779018e23f1ed60b1f0ab9d3b0477e3779ff
admin <make75682@gmail.com>
2022-07-08 16:24:18 +0800

    filte course amoount

--- web/course.php
+++ web/course.php
@@ -1,11 +1,24 @@
 <?php
 require_once("../db-connect.php");
 
+$amount_limit=$_GET["amount-limit"]??5;
+$page=$_GET["page"]??1;
+
 $stmtCategory=$db_host->prepare("SELECT * FROM category");
 $stmtCategory->execute();
 $rowsCategory=$stmtCategory->fetchALL(PDO::FETCH_ASSOC);
 
-$stmt=$db_host->prepare("SELECT course.id,course_img.img_name,name,category.category_en_name,amount,price,sold_amount,state FROM course JOIN category ON course.category_id=category.id JOIN course_img ON course.id=course_img.course_id GROUP BY course_id ");
+$stmtAll=$db_host->prepare("SELECT * FROM course JOIN course_img ON course.id=course_img.course_id GROUP BY course_id");
+$stmtAll->execute();
+$courseAmount=count($stmtAll->fetchALL(PDO::FETCH_ASSOC));
+// echo $courseAmount;
+$start=($page-1)*$amount_limit;
+if($courseAmount%$amount_limit===0)
+$totalAmount=floor($courseAmount/$amount_limit);
+else
+$totalAmount=floor($courseAmount/$amount_limit)+1;
+
+$stmt=$db_host->prepare("SELECT course.id,course_img.img_name,name,category.category_en_name,amount,price,sold_amount,state FROM course JOIN category ON course.category_id=category.id JOIN course_img ON course.id=course_img.course_id GROUP BY course_id LIMIT $start,$amount_limit");
 $stmt->execute();    
 $rows=$stmt->fetchALL(PDO::FETCH_ASSOC);
 
@@ -37,13 +50,15 @@ $rows=$stmt->fetchALL(PDO::FETCH_ASSOC);
         <div class="container-fluid">
             <div class="d-flex justify-content-between">
                 <p class="title">課程管理</p>
+                <form action="product.php" method="get">
                 <p>顯示 
-                  <select id="amount-limit" class="count-bg text-center" aria-label="Default select example">
-                    <option value="5" selected>5</option>
-                    <option value="10">10</option>
+                  <select id="amount-limit" class="count-bg text-center" name="amount-limit"  onchange="submit()">
+                    <option value="5" <?= $select=$amount_limit==5?'selected':'' ?>>5</option>
+                    <option value="10"<?= $select=$amount_limit==10?'selected':'' ?>>10</option>
                   </select> 
                   筆數
                 </p>
+                </form>                
             </div>
             <?php require("./mod/search-bar.php") ?>
             <div class="text-end my-4">
@@ -76,16 +91,31 @@ $rows=$stmt->fetchALL(PDO::FETCH_ASSOC);
                     </td>
                     <td><?=$row["amount"]?></td>
                     <td><?=$row["price"]?></td>
-                    <td><?=$row["sold_amount"]?></td>
-                    <?php if($row["state"]==1)$checked='checked' ?>
-                    <td><input type="checkbox" <?= $checked??""?> disabled></td>
+                    <td><?=$row["sold_amount"]?></td>                    
+                    <td><input type="checkbox" <?php if($row["state"]==1)print("checked") ?> disabled></td>
                     <td></td>
                     <td><a href="view-course.php?course=<?=$row["id"]?>"><i class="fa-solid fa-pen"></i></a></td>
                   </tr>
                   <?php endforeach; ?>
               </tbody>
             </table>
-            <?php require("./mod/page-number.php") ?>
+            <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-center">
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                        <span aria-hidden="true">
+                            <</span>
+                    </a>
+                </li>
+                  <?php for($i=1;$i<=$totalAmount;$i++): ?>
+                <li class="page-item"><a class="page-link active" href="product.php?amount-limit=<?=$amount_limit?>&page=<?=$i?>"><?=$i?></a></li>
+                  <?php endfor; ?>
+                    <a class="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">></span>
+                    </a>
+                </li>
+            </ul>
+            </nav>
         </div>
     </main>
 
