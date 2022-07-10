6cd78ae82600d105877c143045f5aadb33c2fba9
angushyx <sickmi14798@gmail.com>
2022-07-10 19:44:14 +0800

    blog can add

--- web/blog/blog-page.php
+++ web/blog/blog-page.php
@@ -1,13 +1,29 @@
 <?php
-// echo 拿到各自的 id後顯示該篇部落格;
+require_once("../../db-connect.php");
 
+$current_id=$_GET["id"];
+
+$stmt=$db_host->prepare("SELECT * FROM blog WHERE id=$current_id");
+
+try {
+    $stmt->execute();
+    $blog = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "預處理陳述式執行失敗！ <br/>";
+    echo "Error: " . $e->getMessage() . "<br/>";
+    $db_host = NULL;
+    exit;
+}
+
+
+$db_host = NULL;
 
 ?>
 
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Title</title>
+    <title>Blog page</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -33,15 +49,19 @@
     </header>
 
     <div class="container mt-5 text-center" style="">
-        <div class="text-center text-center"> 發布於2019-05-15 10:06:47</div>
+        <div class="text-center text-center"> 發布於    
+            <?php
+                $date=new DateTime($blog[0]["create_time"]);
+                echo  $date->format('M-d-Y H:i:s');
+            ?></div>
  
         
-         <h3 class="text-center my-4">親手製作一件獨一無二的金屬飾品</h3>   
+         <h3 class="text-center my-4"><?=$blog[0]["title"]?></h3>   
         <h5 >by 黑色小花貓</h5>    
          <div class="text-center my-4">
-            <span class="badge bg-secondary">#Secondary</span>
-            <span class="badge bg-secondary">#Secondary</span>
-            <span class="badge bg-secondary">#Secondary</span>
+            <span class="badge bg-secondary"><?=$blog[0]["title"]?></span>
+            <span class="badge bg-secondary"><?=$blog[0]["title"]?></span>
+            <span class="badge bg-secondary"><?=$blog[0]["title"]?></span>
         </div>
         <hr>
         
@@ -50,18 +70,23 @@
        <img src="https://images.unsplash.com/photo-1600857544200-b2f666a9a2ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80 " class="img-thumbnail my-5 rounded w-75" style="width:"200px" alt="...">
 
        
-
+    <div class='text-start'>
         <article>
-        自大街向小巷復行數十步，不遠處的一扇門外，佈滿綠油油的中大型盆景，從石屎森林走進慢活叢林，逃離現實的「以覺學INTZUITION」工作室，到了。
-
-        與友人走過門外的綠色小叢林推門而進，感覺有如進了魔戒中哈比人的小屋，眼前全木的裝潢和擺設，要與數分鐘前所置身的世界完全隔絕，「以覺學INTZUITION」工作室的環境、氣圍立即使人放鬆下來。
-        溫柔的導師為我們每人送上了一杯茶，然後，慢慢地向我們介紹「以覺學INTZUITION」各個系列的首飾及器物，最大的共通點就是簡約但不簡單，而且堅持了所有的材質均100%可回收，不使用任何電鍍，全手工製作，另外，品牌著重「永續性」，強調給大家工藝的種子，各自種出自己的風景，可選擇金、銀、銅的「種子」，以鑄造及鍛造的工法栽培。不過最引人入勝的莫過於能親手製作一件獨一無二的、高難度又神秘的金屬飾品！
-
-        「以覺學INTZUITION」工作室的環境，是先讓身心放緩，然後在認識品牌後，參與其中，樂在其中。導師讓我們先看部分製成品，簡介材質及過程，然後就為我們準備好原材料，體驗正式開始。眼前看著一條正方體的純銀原材料，腦裡可說是一片空白，如何才能讓一條約銀變成手鐲？到底扭紋是如何製成的？導師細緻地教導每一個步驟，先是練習如何使用刻字的工具，然後是用火槍將原材料軟化，再以工具輔助，製作紋路等等，製成自己想要的模樣，導師在製作過程中，會一直協助並講解，是溫柔、細心、一絲不苟的。這樣使製成品一方面飽含濃厚個性，另一方面又保證了質量；在這幽靜的空間，精神狀態得以放鬆，呼吸著植物氣息，聆聽著工具聲、呼吸聲，專注著自己用力又用心的製成品，喜歡、喜愛。
-
-        在「以覺學INTZUITION」的三小時過去，獨一無二的專屬成品面世，這幾個小時的收獲，與環境中的一切互動，非單純的飾品製作，而是用心生活的體驗。工作室門外的自己，就先留在門外，遲下再見。
+       <?=$blog[0]["content"]?>
         </article>
-
+        <article>
+       <?=$blog[0]["content"]?>
+        </article>
+        <article>
+       <?=$blog[0]["content"]?>
+        </article>
+        <article>
+       <?=$blog[0]["content"]?>
+        </article>
+        <article>
+       <?=$blog[0]["content"]?>
+        </article>
+    </div>      
         <div class="d-flex mt-5 gap-5 justify-content-around">
             <div class="card" style="width: 18rem;">
                 <img style="height: 200px   ;" src="https://images.unsplash.com/photo-1631125915902-d8abe9225ff2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aGFuZG1hZGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
