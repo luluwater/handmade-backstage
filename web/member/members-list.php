ede5a7ef1db52ac950f5c8883747019c1c7849a1
lulu <water0452@gmail.com>
2022-07-13 13:54:39 +0800

    0713 pagelink update

--- web/member/members-list.php
+++ web/member/members-list.php
@@ -156,7 +156,7 @@ require("../../db-connect.php");
                         </li>
                     <?php endfor; ?>
                         <li class="page-item">
-                            <a class="page-link" href="<?=orderLink("nextPage",$pageView,$order)?>&<?= $theNextPage ?>" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>
+                            <a class="page-link" href="<?=orderLink("nextPage",$pageView, $order) ?>&page=<?= $page + 1 > $totalPage ? $page = $totalPage : $page = $page + 1 ?>" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>
                         </li>
                     <li class="px-5 py-2">
                         第<?= $startItem ?>- <?= $endItem ?>筆,共 <?= $memberCount ?> 筆資料
